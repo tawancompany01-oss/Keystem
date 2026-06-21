a520b49ff312ef196f43be503c3814ae43ce77a9
tawancompany01-oss <tawancompany01@gmail.com>
2026-06-21 15:05:26 +0700

    Update Key.lua

--- Key.lua
+++ Key.lua
@@ -23,7 +23,7 @@ local Config = {
     Secret          = "589452", -- This makes the script ONLY run from the key script. Even if they copy the original obfuscated script to bypass the key, they won't be able to!
     
     -- [3] Scripts & Links
-    MainScriptURL   = "https://raw.githubusercontent.com/tawancompany01-oss/Keystem/refs/heads/main/JubMrua.lua", -- The raw URL of your main script
+    MainScriptURL   = "https://raw.githubusercontent.com/tawancompany01-oss/Keystem/refs/heads/main/Script.lua", -- The raw URL of your main script
     
     -- [4] Social Media Settings (Set to true to show, false to hide)
     ShowDiscord     = false,
